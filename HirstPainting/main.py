b508866275a144cede92f9aac60ae8f1d94a4b7c
Ronil Christian <ronilrc13@gmail.com>
2022-08-02 09:29:19 -0500

    day 18

---
+++ HirstPainting/main.py
@@ -0,0 +1,45 @@
+import colorgram
+import turtle as t
+import random
+
+# Extract 6 colors from an image.
+colors = colorgram.extract('image.jpg', 30)
+
+rgb_colors = []
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+# print(rgb_colors)
+
+color_list = [(236, 224, 80), (197, 7, 71), (195, 164, 13), (201, 75, 15), (231, 54, 132), (110, 179, 216), (217, 163, 101), (27, 105, 168), (35, 186, 109), (19, 29, 168), (13, 23, 66), (212, 135, 177), (233, 223, 7), (199, 33, 132), (13, 183, 212), (230, 166, 199), (126, 189, 162), (8, 49, 28), (40, 132, 77), (128, 219, 232), (58, 12, 25), (67, 22, 7), (114, 90, 210), (146, 216, 199), (179, 17, 8), (233, 66, 34)]
+
+t.colormode(255)
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(223)
+tim.forward(305)
+tim.setheading(0)
+
+num_of_dots = 100
+
+for dot_count in range (1, num_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = t.Screen()
+screen.exitonclick()
