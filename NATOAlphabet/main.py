786bb0d4a3f5bf7437566e12d9bffde4b7548485
Ronil Christian <ronilrc13@gmail.com>
2022-11-01 23:59:30 -0500

    day 30

--- NATOAlphabet/main.py
+++ NATOAlphabet/main.py
@@ -6,6 +6,14 @@ dataframe = pd.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index, row) in dataframe.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-result = [phonetic_dict[letter] for letter in word]
-print(result)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        result = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()
--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,4 +1,5 @@
 # import pyperclip
+import json
 from random import choice, randint, shuffle
 from tkinter import *
 from tkinter import messagebox
@@ -26,24 +27,42 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops!", message="Please make sure you haven't left any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title="cConfirm your details", message=f"These are the details entered:\n"
-                                                                              f"Email: {email}\n"
-                                                                              f"Password: {password}\n"
-                                                                              f"Do you want to save?")
+        try:
+            with open("data.json", "r") as f:
+                # reading new data
+                data = json.load(f)
+        except FileNotFoundError:
+            with open("data.json", "w") as f:
+                # saving new data
+                json.dump(new_data, f, indent=4)
+        else:
+            # updating old data with new data
+            data.update(new_data)
 
-        if is_ok:
-            with open("data.txt", "a") as f:
-                f.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+            with open("data.json", "w") as f:
+                # saving updated data
+                json.dump(data, f, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def find_password():
+    pass
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title("Pasword Manager")
+window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
@@ -62,15 +81,15 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry = Entry()
+website_entry.grid(row=1, column=1, sticky="EW")
 website_entry.focus()
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
 email_entry.insert(0, "email@example.com")
 
-password_entry = Entry(width=21)
+password_entry = Entry()
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
@@ -79,5 +98,8 @@ generate_password_button.grid(row=3, column=2, sticky="EW")
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
+
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2, sticky="EW")
 
 window.mainloop()
